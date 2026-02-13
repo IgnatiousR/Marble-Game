3c93fe464842835686a4405f2e165b5bbf21fb62
Ignatious Rozario <ignatiousr80@gmail.com>
2026-02-13 22:29:29 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import './index.css'
-import Experience from './Experience'
-import { KeyboardControls } from '@react-three/drei'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import "./index.css";
+import Experience from "./Experience";
+import { KeyboardControls } from "@react-three/drei";
 
 // createRoot(document.getElementById('root')!).render(
 //   <StrictMode>
@@ -12,33 +12,34 @@ import { KeyboardControls } from '@react-three/drei'
 //     </Canvas>
 //   </StrictMode>
 // )
-const root = createRoot(document.querySelector('#root') as HTMLElement)
+const root = createRoot(document.querySelector("#root") as HTMLElement);
 
 root.render(
   <StrictMode>
     <KeyboardControls
-        map={[
-            { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
-            { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
-            { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
-            { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
-            { name: 'restart', keys: ['Enter', 'KeyR'] },
-            { name: 'menu', keys: ['Escape'] },
-            { name: 'jump', keys: ['Space'] },
-        ]}
+      map={[
+        { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        { name: "backward", keys: ["ArrowDown", "KeyS"] },
+        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+        { name: "restart", keys: ["Enter", "KeyR"] },
+        { name: "menu", keys: ["Escape"] },
+        { name: "jump", keys: ["Space"] },
+      ]}
     >
-        <Canvas
-            shadows
-            camera={{
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [2.5, 4, 6]
-            }}
-        >
-            <Experience />
-        </Canvas>
-        {/* <Interface /> */}
+      <Canvas
+        shadows
+        gl={{ antialias: true }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [2.5, 4, 6],
+        }}
+      >
+        <Experience />
+      </Canvas>
+      {/* <Interface /> */}
     </KeyboardControls>
-    </StrictMode>
-)
+  </StrictMode>,
+);
