b7f8f8e1a8f37a525ea5fa8bf579c97d8abb32b5
Ignatious Rozario <ignatiousr80@gmail.com>
2026-02-13 15:37:05 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import * as THREE from "three";
+import { Leva, LevaPanel, useControls } from "leva";
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <mesh></mesh>
+    </>
+  )
+}
+
+export default App
---
+++ src/Experience.tsx
@@ -0,0 +1,16 @@
+import Lights from "./Lights";
+import Level from "./Level";
+import { OrbitControls } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
+
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls makeDefault />
+      <Physics debug>
+        <Lights />
+        <Level />
+      </Physics>
+    </>
+  );
+}
