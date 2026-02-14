04d8b0fc2d8a35fc6d106434b49390d292d19912
Ignatious <ignatiousr80@gmail.com>
2026-02-14 17:34:07 +0600

    added player to scene

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -2,6 +2,7 @@ import Lights from "./Lights";
 import Level from "./Level";
 import { OrbitControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
+import { Player } from "./Player";
 
 export default function Experience() {
   return (
@@ -10,6 +11,7 @@ export default function Experience() {
       <Physics debug>
         <Lights />
         <Level />
+        <Player position={[0, 1, 0]} />
       </Physics>
     </>
   );
