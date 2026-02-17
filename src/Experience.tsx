f1bef8a34dd714c9c925fc1dabd7c2f07c39f37a
Ignatious <ignatiousr80@gmail.com>
2026-02-17 10:02:06 +0600

    removed orbitcontrols

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,13 +1,13 @@
 import Lights from "./Lights";
 import Level from "./Level";
-import { OrbitControls } from "@react-three/drei";
+// import { OrbitControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import { Player } from "./Player";
 
 export default function Experience() {
   return (
     <>
-      <OrbitControls makeDefault />
+      {/* <OrbitControls makeDefault /> */}
       <Physics debug>
         <Lights />
         <Level />
