603b4f7432d75b278223b54915ff18086799cfee
Ignatious <ignatiousr80@gmail.com>
2026-02-19 15:18:55 +0600

    added blockCount state

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -3,14 +3,20 @@ import Level from "./Level";
 // import { OrbitControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import { Player } from "./Player";
+import { useGame } from "./stores/useGame";
 
 export default function Experience() {
+  const blockCount = useGame((state) => {
+    return state.blockCount;
+  });
+  // console.log(blockCount);
+
   return (
     <>
       {/* <OrbitControls makeDefault /> */}
       <Physics debug>
         <Lights />
-        <Level />
+        <Level count={blockCount} />
         <Player position={[0, 1, 0]} />
       </Physics>
     </>
