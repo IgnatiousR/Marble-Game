411970c66fac40075c7d4a85a8a9aa19467b5ebf
Ignatious Rozario <ignatiousr80@gmail.com>
2026-02-21 22:14:19 +0600

    added new states

--- src/stores/useGame.ts
+++ src/stores/useGame.ts
@@ -1,28 +1,44 @@
 import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
 
 interface GameState {
   blockCount: number;
+  blocksSeed: number;
+  startTime: number;
+  endTime:  number;
   phase: "ready" | "playing" | "ended";
   start: () => void;
   restart: () => void;
   end: () => void;
 }
 
-export const useGame = create<GameState>((set) => ({
+export const useGame = create<GameState>()(subscribeWithSelector((set) => ({
   blockCount: 4,
+  blocksSeed: 0,
+  startTime:0,
+  endTime:0,
   phase: "ready",
-
-  // start: () => {
-  //   console.log("Playing");
-  //   set(() => {
-  //     return { phase: "playing" };
-  //   });
-  // },
 
   start: () => {
     console.log("Playing");
-    set({ phase: "playing" });
+    set((state) => {
+      if(state.phase === 'ready') return { phase: "playing", startTime: Date.now() };
+      return {}
+    });
   },
-  restart: () => set({ phase: "ready" }),
-  end: () => set({ phase: "ended" }),
-}));
+
+  // start: () => {
+  //   console.log("Playing");
+  //   set({ phase: "playing" });
+  // },
+  restart: () => {
+    set((state) => {
+      if(state.phase === 'playing' || state.phase == 'ended' ) return { phase: "ready", blocksSeed:Math.random() };
+      return {}
+    })
+  },
+  end: () => set((state) => {
+      if(state.phase === 'playing' ) return { phase: "ended", endTime: Date.now() };
+      return {}
+    }),
+})));
