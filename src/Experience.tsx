3bb7dd8e5eb47f2b081e6efc1223879c97c322ae
Ignatious Rozario <ignatiousr80@gmail.com>
2026-02-21 22:30:31 +0600

    Update Experience.tsx

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -4,21 +4,26 @@ import Level from "./Level";
 import { Physics } from "@react-three/rapier";
 import { Player } from "./Player";
 import { useGame } from "./stores/useGame";
+import { Effects } from "./Effects";
 
 export default function Experience() {
-  const blockCount = useGame((state) => {
-    return state.blockCount;
-  });
+  const blockCount = useGame((state) => state.blockCount);
+  const blocksSeed = useGame((state) => state.blocksSeed);
   // console.log(blockCount);
 
   return (
     <>
+      <color args={["#252731"]} attach={"background"} />
       {/* <OrbitControls makeDefault /> */}
-      <Physics debug>
+      <Physics
+      // debug
+      >
         <Lights />
-        <Level count={blockCount} />
+        <Level count={blockCount} seed={blocksSeed} />
         <Player position={[0, 1, 0]} />
       </Physics>
+
+      <Effects />
     </>
   );
 }
