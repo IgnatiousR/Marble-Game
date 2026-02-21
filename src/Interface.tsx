b18e99f3df516f017c8f711790a9a07b2bb92d8c
Ignatious Rozario <ignatiousr80@gmail.com>
2026-02-21 22:30:22 +0600

    Update Interface.tsx

--- src/Interface.tsx
+++ src/Interface.tsx
@@ -1,4 +1,7 @@
 import { useKeyboardControls } from "@react-three/drei";
+import { useGame } from "./stores/useGame";
+import { useEffect, useRef } from "react";
+import { addEffect } from "@react-three/fiber";
 
 export const Interface = () => {
   const forward = useKeyboardControls((state) => state.forward);
@@ -6,15 +9,45 @@ export const Interface = () => {
   const leftward = useKeyboardControls((state) => state.leftward);
   const rightward = useKeyboardControls((state) => state.rightward);
   const jump = useKeyboardControls((state) => state.jump);
-  // console.log(controls);
+
+  const phase = useGame((state) => state.phase);
+  const restart = useGame((state) => state.restart);
+  const time = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const unsubscribeEffect = addEffect(() => {
+      const state = useGame.getState();
+
+      let elapsedTime = 0;
+
+      if (state.phase === "playing") elapsedTime = Date.now() - state.startTime;
+      else if (state.phase === "ended")
+        elapsedTime = state.endTime - state.startTime;
+
+      elapsedTime /= 1000;
+      elapsedTime = Number(elapsedTime.toFixed(2));
+
+      if (time.current) time.current.textContent = String(elapsedTime);
+    });
+
+    return () => {
+      unsubscribeEffect();
+    };
+  });
 
   return (
     <div className="interface">
       {/* Time */}
-      <div className="time">0.00</div>
+      <div ref={time} className="time">
+        0.00
+      </div>
 
       {/* Restart */}
-      <div className="restart">Restart</div>
+      {phase === "ended" && (
+        <div className="restart" onClick={restart}>
+          Restart
+        </div>
+      )}
 
       {/* Controls */}
       <div className="controls">
